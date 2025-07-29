1493e95d7eb8855cba557206987ebfd22d94647f
cejoji <bensonfran@gmail.com>
2025-07-29 13:50:46 +0200

    Update server.js
    added 
    
    // Routes
    const duffelRoutes = require('./routes/duffelRoutes');
    app.use('/', duffelRoutes);
    
    // Port
    const PORT = process.env.PORT || 4000;
    app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`);
    });

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingsRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/admin', adminRoutes);
+const duffelRoutes = require('./routes/duffelRoutes');
+app.use('/', duffelRoutes);
+
 
 app.get('/api/ping', (req, res) => {
   res.send('Backend is working!');
@@ -31,3 +34,9 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   })
   .catch(err => console.log(err));
+
+// Port
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
